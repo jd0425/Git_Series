56cfcfe6efaf3b9356686683997e5941a6d2abb4
jd0425 <jamesdobbs@gmail.com>
2026-07-25 10:18:54 -0400

    Second commit with updated file

--- First_File.py
+++ First_File.py
@@ -1,4 +1,5 @@
 # This is a new file and our first commit
 
-# https://youtu.be/lLoJHifWTRw?is=3zbu76j3uIPggk2T
-# this is the mobile link to the lesson Alex the Analyst: Learn Git and GitHub in 1 Hour!
+#This is our code
+
+print("I love Git")
